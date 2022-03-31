e7ba2950a723651c2ac31ba4d35cb90f33373482
tayee <zhanghaojie@mininglamp.com>
2022-03-31 17:57:08 +0800

    refresh

--- keras2bert/backend.py
+++ keras2bert/backend.py
@@ -7,8 +7,10 @@ if 'TF_KERAS' in os.environ and os.environ['TF_KERAS'] != '0':
     import tensorflow
     sys.modules['keras'] = tensorflow.keras
 
+
 import keras
 import keras.backend as K
+
 
 def gelu_tanh(x):
     """gelu激活函数的近似算法.
@@ -17,6 +19,7 @@ def gelu_tanh(x):
     """
     return 0.5 * x * (1.0 + K.tanh(np.sqrt(2.0 / np.pi) * (x + 0.044715 * x * x * x)))
 
+
 def gelu_erf(x):
     """gelu激活函数的精确算法.
      # 引用:
@@ -24,12 +27,14 @@ def gelu_erf(x):
     """
     return 0.5 * x * (1.0 + tf.math.erf(x / np.sqrt(2)))
 
+
 def softmax(x, axis=-1):
     """自定义softmax激活函数.
     """
     x = x - np.max(x, axis=axis, keepdims=True)
     y = np.exp(x)
     return y / y.sum(axis=axis, keepdims=True)
+
 
 def mask_sequences(x, mask, axis=1, value=None):
     """用于mask序列.
@@ -47,6 +52,7 @@ def mask_sequences(x, mask, axis=1, value=None):
         mask = K.expand_dims(mask, -1)
     x = x * mask + value * (1 - mask)
     return x
+
 
 def pad_sequences(sequences,
                   axis=1,
@@ -91,6 +97,7 @@ def pad_sequences(sequences,
             pad_seq.append(seq)
     return np.asarray(pad_seq, dtype=object)
 
+
 class Sinusoidal(keras.initializers.Initializer):
     """Sin-Cos 位置嵌入初始化器.
     # 引用:
@@ -110,12 +117,15 @@ class Sinusoidal(keras.initializers.Initializer):
 
 _INFINITY = 1e12
 
+
 def infinity():
     return _INFINITY
+
 
 def set_infinity(value):
     global _INFINITY
     _INFINITY = value
+
 
 def identity(x):
     return x
