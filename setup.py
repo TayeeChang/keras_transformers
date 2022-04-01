b89a435c828b191a7b7475c55d98a88b02b90bfa
tayee <zhanghaojie@mininglamp.com>
2022-04-01 10:42:14 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+import re
+from setuptools import setup, find_packages
+
+current_path = os.path.abspath(os.path.dirname(__file__))
+
+
+def read_file(*parts):
+    with open(os.path.join(current_path, *parts)) as reader:
+        return reader.read()
+
+
+def get_requirements(*parts):
+    with open(os.path.join(current_path, *parts)) as reader:
+        return list(map(lambda x: x.strip(), reader.readlines()))
+
+
+def find_version(*file_paths):
+    version_file = read_file(*file_paths)
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
+
+setup(
+    name='keras2bert',
+    version=find_version('keras2bert', 'models', '__init__.py'),
+    packages=find_packages(),
+    url='https://github.com/TayeeChang/keras2bert',
+    license='MIT',
+    author='keras2bert',
+    author_email='TayeeChang@gmail.com',
+    description='Transformer Family with Keras',
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
+    install_requires=get_requirements('requirements.txt'),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
